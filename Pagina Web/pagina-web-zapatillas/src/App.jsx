b2fd4a3fa8a72805899a61cd07573119c59ecd84
tricky <bernabeacostamelnik2006@gmail.com>
2025-09-30 09:37:45 -0300

    creacion del error 404

--- Pagina Web/pagina-web-zapatillas/src/App.jsx
+++ Pagina Web/pagina-web-zapatillas/src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import Navbar from './components/NavBar'
+import React from "react";
+import Navbar from './components/NavBar' 
+import Error from './components/error'
 
 
 export const App = () => {
@@ -7,8 +8,8 @@ export const App = () => {
     <>
       <div className="App">
       <Navbar />
-      
-      <main style={{ padding: '24px' }}>
+            
+        <main style={{ padding: '24px' }}>
         <h1>Bienvenido a mi sitio</h1>
         <p>Contenido de tu página...</p>
       </main>
@@ -16,3 +17,5 @@ export const App = () => {
     </>
   )
 }
+
+
