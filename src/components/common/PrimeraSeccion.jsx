f9e197d2970fb97ca2f2ce4ee22dcdb4bc49b5f9
email <leon_mtb@hotmail.com>
2025-10-07 19:14:14 -0300

    Nueva actualizacion

--- src/components/common/PrimeraSeccion.jsx
+++ src/components/common/PrimeraSeccion.jsx
@@ -9,42 +9,42 @@ export const PrimeraSeccion = () => {
     
     <div class="categoria-card">
       <div class="imagen-circulo">
-        <img src="mujer.jpg" alt="Mujer"/>
+        <img src="https://brand.assets.adidas.com/image/upload/f_auto,q_auto:best,fl_lossy/if_w_gt_400,w_400/mujer_asset_d_0d77cee49c.jpg" alt="Mujer"/>
       </div>
       <p class="categoria-texto">MUJER</p>
     </div>
 
     <div class="categoria-card">
       <div class="imagen-circulo">
-        <img src="hombre.jpg" alt="Hombre"/>
+        <img src="https://brand.assets.adidas.com/image/upload/f_auto,q_auto:best,fl_lossy/if_w_gt_400,w_400/hombre_asset_d_7bb656194a.jpg" alt="Hombre"/>
       </div>
       <p class="categoria-texto">HOMBRE</p>
     </div>
 
     <div class="categoria-card">
       <div class="imagen-circulo">
-        <img src="nina.jpg" alt="Niña"/>
+        <img src="https://brand.assets.adidas.com/image/upload/f_auto,q_auto:best,fl_lossy/if_w_gt_400,w_400/nina_asset_d_90cd005d28.jpg" alt="Niña"/>
       </div>
       <p class="categoria-texto">NIÑA</p>
     </div>
 
     <div class="categoria-card">
       <div class="imagen-circulo">
-        <img src="nino.jpg" alt="Niño"/>
+        <img src="https://brand.assets.adidas.com/image/upload/f_auto,q_auto:best,fl_lossy/if_w_gt_400,w_400/nino_asset_d_eea9acd5bf.jpg" alt="Niño"/>
       </div>
       <p class="categoria-texto">NIÑO</p>
     </div>
 
     <div class="categoria-card seleccionada">
       <div class="imagen-circulo">
-        <img src="adolescentes.jpg" alt="Adolescentes"/>
+        <img src="https://brand.assets.adidas.com/image/upload/f_auto,q_auto:best,fl_lossy/if_w_gt_400,w_400/adolescentes_d_96337a4059.jpg" alt="Adolescentes"/>
       </div>
       <p class="categoria-texto">ADOLESCENTES</p>
     </div>
 
     <div class="categoria-card">
       <div class="imagen-circulo">
-        <img src="ver-todo.jpg" alt="Ver todo"/>
+        <img src="https://brand.assets.adidas.com/image/upload/f_auto,q_auto:best,fl_lossy/if_w_gt_400,w_400/ver_todo_d_13f23273ac.jpg" alt="Ver todo"/>
       </div>
       <p class="categoria-texto">VER TODO</p>
     </div>
