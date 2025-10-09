9b346ced8a3df9e6ce2046031d478c87f156c815
pablo99j <pablo65tg@gmail.com>
2025-10-09 19:37:16 -0300

    Se agrega libreria boostrap y se inicia seccion de descuentos

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { PrimeraSeccion } from '../common/Primeraseccion'
+import SeccionDescuento from '../SeccionDescuento'
 
 export const Inicio = () => {
   return (
     <div>
       <PrimeraSeccion/>
+      <SeccionDescuento/>
   
     </div>
   )
 }
+
+
